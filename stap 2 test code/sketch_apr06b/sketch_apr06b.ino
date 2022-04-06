1f0d6f191127db89d10e07cea055272d37646fb1
Rudieee <74548007+Rudieee@users.noreply.github.com>
2022-04-06 10:18:53 +0200

    Add files via upload

---
+++ stap 2 test code/sketch_apr06b/sketch_apr06b.ino
@@ -0,0 +1,82 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+int knopGeel = 13;
+int knopBlauw = 12;
+int knopGroen = 11;
+int knopRood = 10;
+int knopWit = 9;
+int ledGeel = 8;
+int ledGroen = 6;
+int ledRood = 7;
+int ledBlauw = 5;
+int ledPin = LED_BUILTIN;
+int knopStatusGeel = LOW;
+int knopStatusBlauw = LOW;
+int knopStatusGroen = LOW;
+int knopStatusRood = LOW;
+int knopStatusWit = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(knopGeel, INPUT);
+  pinMode(knopBlauw, INPUT);
+  pinMode(knopGroen, INPUT);
+  pinMode(knopRood, INPUT);
+  pinMode(knopWit, INPUT);
+  pinMode(ledGeel, OUTPUT);
+  pinMode(ledGroen, OUTPUT);
+  pinMode(ledRood, OUTPUT);
+  pinMode(ledBlauw, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  lcd.begin(16, 2);
+
+  lcd.setCursor(0, 0);
+  for (int i = 0; i < 10; i++) {
+    lcd.print(i);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  knopStatusGeel = digitalRead(knopGeel);
+  knopStatusBlauw = digitalRead(knopBlauw);
+  knopStatusGroen = digitalRead(knopGroen);
+  knopStatusRood = digitalRead(knopRood);
+  knopStatusWit = digitalRead(knopWit);
+
+
+  if (knopStatusGeel == HIGH) {
+    digitalWrite(ledGeel, HIGH);
+  } else {
+    digitalWrite(ledGeel, LOW);
+  }
+
+  if (knopStatusBlauw == HIGH) {
+    digitalWrite(ledBlauw, HIGH);
+  } else {
+    digitalWrite(ledBlauw, LOW);
+  }
+
+  if (knopStatusGroen == HIGH) {
+    digitalWrite(ledGroen, HIGH);
+  } else {
+    digitalWrite(ledGroen, LOW);
+  }
+
+  if (knopStatusRood == HIGH) {
+    digitalWrite(ledRood, HIGH);
+  } else {
+    digitalWrite(ledRood, LOW);
+  }
+
+ if (knopStatusWit == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+
+}
